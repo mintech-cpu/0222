7f90dfb38c43837d4de0a8e8ba00ec2e3ab3666f
mintech-cpu <78586667+mintech-cpu@users.noreply.github.com>
2021-02-22 22:13:34 +0900

    Add files via upload

---
+++ website_views.py
@@ -0,0 +1,17 @@
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+    
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt['username'] = 'みちゃん'
+        return ctxt
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+    
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt['num_services'] = 12345678
+        return ctxt
